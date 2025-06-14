cf2fd615a7ccca8810ebb63a8672f0e5519be6d5
Aisyah Humaira <asyhhu@korea.ac.kr>
2025-06-14 20:58:50 +0900

    Update main.py - add selection_sort function

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def linear_search(arr, target):
         if arr[i] == target:
             return i
     return -1
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]

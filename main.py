9fad80adb3e5c071897fa22c3ff1a598102c5cc2
Aisyah Humaira <asyhhu@korea.ac.kr>
2025-06-14 20:57:02 +0900

    Update main.py - add linear_search function

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def bubble_sort(arr):
         for j in range(0, len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
